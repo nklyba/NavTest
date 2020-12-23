11cc77fb745184eb74362aa9c1186efc8fc2a033
nklyba <n.klyba@gmail.com>
2020-12-23 13:35:55 +0300

    navigation to closed deals

--- app/src/main/java/com/space307/navtest/fragments/DealsListFragment.kt
+++ app/src/main/java/com/space307/navtest/fragments/DealsListFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.space307.navtest.R
 import com.space307.navtest.utils.getNavigationBarTabFromGraphId
 import com.space307.navtest.utils.setupWithNavController
@@ -16,6 +19,26 @@ class DealsListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_deals_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_deals_list, container, false)
+
+        view.findViewById<Button>(R.id.active_op_deal_button).setOnClickListener {
+            Toast.makeText(requireContext(), "TODO Navigate to op trading", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        view.findViewById<Button>(R.id.active_fx_deal_button).setOnClickListener {
+            Toast.makeText(requireContext(), "TODO Navigate to fx trading", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        view.findViewById<Button>(R.id.closed_op_deal_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToOpClosedDealBottomSheet)
+        }
+
+        view.findViewById<Button>(R.id.closed_fx_deal_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToFxClosedDealBottomSheet)
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/com/space307/navtest/fragments/FxClosedDealBottomSheetDialog.kt
@@ -0,0 +1,19 @@
+package com.space307.navtest.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.space307.navtest.R
+
+class FxClosedDealBottomSheetDialog: BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet_closed_deal_fx, container, false)
+    }
+
+}
