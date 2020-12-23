00e83189912fd19df357ca9045c6031d31b0f548
nklyba <n.klyba@gmail.com>
2020-12-23 11:23:29 +0300

    navigation for tab more

--- app/src/main/java/com/space307/navtest/fragments/MoreFragment.kt
+++ app/src/main/java/com/space307/navtest/fragments/MoreFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.space307.navtest.R
-import com.space307.navtest.utils.getNavigationBarTabFromGraphId
-import com.space307.navtest.utils.setupWithNavController
-import com.space307.navtest.views.NavigationBarView
 
 class MoreFragment : Fragment() {
 
@@ -16,6 +15,20 @@ class MoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        val view = inflater.inflate(R.layout.fragment_more, container, false)
+
+        view.findViewById<Button>(R.id.deposit_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToDeposit)
+        }
+
+        view.findViewById<Button>(R.id.withdraw_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToWithdraw)
+        }
+
+        view.findViewById<Button>(R.id.settings_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToSettings)
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/space307/navtest/fragments/SettingsFragment.kt
+++ app/src/main/java/com/space307/navtest/fragments/SettingsFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.space307.navtest.R
-import com.space307.navtest.utils.getNavigationBarTabFromGraphId
-import com.space307.navtest.utils.setupWithNavController
-import com.space307.navtest.views.NavigationBarView
 
 class SettingsFragment : Fragment() {
 
@@ -16,6 +15,12 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        view.findViewById<Button>(R.id.sound_packs_button).setOnClickListener {
+            findNavController().navigate(R.id.navigateToSoundPacks)
+        }
+
+        return view
     }
 }
